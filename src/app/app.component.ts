f94f07cfc65dd96df032e194b8f61d8daad8bbcf
shradha <shradhagupta1499@gmail.com>
2022-04-02 19:15:31 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import ArrayWords from '../utils/words'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'word-generator';
+
+words=''
+limit=10;
+
+
+handleSlideChange(newLimit:number){
+
+this.limit=newLimit;
+}
+
+
+generate(){
+
+  this.words=ArrayWords.slice(0,this.limit).join(' ');
+}
 }
